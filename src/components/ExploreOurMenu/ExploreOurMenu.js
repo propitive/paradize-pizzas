50f54b3eb86854122b3fde1f6dcc07fc14514daf
Propitive <propitive@gmail.com>
2023-09-10 15:16:12 -0500

    added header for explore our menu

--- src/components/ExploreOurMenu/ExploreOurMenu.js
+++ src/components/ExploreOurMenu/ExploreOurMenu.js
@@ -4,20 +4,17 @@ function ExploreOurMenu() {
   console.log(explorePhotos);
   return (
     <section className="explore">
-      {explorePhotos.map((item, i) => {
-        // {
-        //   console.log(item);
-        //   console.log(item.image);
-        //   console.log(item.title);
-        //   console.log(explorePhotos);
-        // }
-        return (
-          <div className="explore__item" key={i}>
-            <img className="explore__image" src={item.image} />
-            <h2 className="explore__title"> {item.title} </h2>
-          </div>
-        );
-      })}
+      <h2 className="explore__header">EXPLORE OUR MENU</h2>
+      <div className="explore__items">
+        {explorePhotos.map((item, i) => {
+          return (
+            <div className="explore__item" key={i}>
+              <img className="explore__image" src={item.image} />
+              <h2 className="explore__title"> {item.title} </h2>
+            </div>
+          );
+        })}
+      </div>
     </section>
   );
 }
