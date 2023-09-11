14e46933796765a0b98b59381cc6dae99b9f5f8a
Propitive <propitive@gmail.com>
2023-09-11 09:50:33 -0500

    added border bottom feature to explore our menu

--- src/components/ExploreOurMenu/ExploreOurMenu.js
+++ src/components/ExploreOurMenu/ExploreOurMenu.js
@@ -1,7 +1,7 @@
+import { useState } from "react";
 import { explorePhotos } from "../../utils/constants";
 
 function ExploreOurMenu() {
-  console.log(explorePhotos);
   return (
     <section className="explore">
       <h2 className="explore__header">EXPLORE OUR MENU</h2>
@@ -12,7 +12,7 @@ function ExploreOurMenu() {
               <div className="explore__image-wrapper">
                 <img className="explore__image" src={item.image} />
               </div>
-              <h2 className="explore__title"> {item.title} </h2>
+              <h2 className="explore__title">{item.title}</h2>
             </div>
           );
         })}
