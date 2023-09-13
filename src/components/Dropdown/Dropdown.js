ea9f38e9302abe613085303c2429d326a93c74db
Propitive <propitive@gmail.com>
2023-09-13 16:18:52 -0500

    added dropdown feature

--- src/components/Dropdown/Dropdown.js
+++ src/components/Dropdown/Dropdown.js
@@ -3,13 +3,22 @@ import { menuNavItems } from "../../utils/constants";
 import { Link } from "react-router-dom";
 
 function Dropdown() {
+  const [isDropdown, setIsDropdown] = useState(false);
+
   return (
     <>
-      <ul className="services-submenu">
+      <ul
+        className={isDropdown ? "services-submenu clicked" : "services-submenu"}
+        onClick={() => setIsDropdown(!isDropdown)}
+      >
         {menuNavItems.map((item) => {
           return (
             <li className="dropdown__li" key={item.id}>
-              <Link to={item.path} className={item.className}>
+              <Link
+                to={item.path}
+                className={item.className}
+                onClick={() => setIsDropdown(false)}
+              >
                 {item.title}
               </Link>
             </li>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,33 +1,49 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "../../images/paradizePizzasLogo.svg";
 import { menuNavItems } from "../../utils/constants";
+import Dropdown from "../Dropdown/Dropdown";
 
 function Header() {
+  const [isDropdown, setIsDropdown] = useState(false);
+  const liMenuClassName = isDropdown
+    ? "header__list-item-menu-hovered"
+    : "header__list-item-menu";
+
   return (
-    <header className="header">
-      <Link to="/" style={{ textDecoration: "none", alignSelf: "center" }}>
-        <img className="header__logo" src={logo} />
-      </Link>
-      <ul className="header__list">
-        <li className="header__list-item">MENU</li>
-        <li className="header__list-item">ABOUT</li>
-        <li className="header__list-item">CONTACT US</li>
-        <li className="header__list-item">GALLERY</li>
-      </ul>
-      {/* <button className="header__button">BOOK ONLINE</button> */}
-      <Link to="/" style={{ textDecoration: "none", alignSelf: "center" }}>
-        <button className="header__button">
-          BOOK ONLINE
-          <svg fill="currentColor" viewBox="0 0 24 24" class="icon">
-            <path
-              clip-rule="evenodd"
-              d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm4.28 10.28a.75.75 0 000-1.06l-3-3a.75.75 0 10-1.06 1.06l1.72 1.72H8.25a.75.75 0 000 1.5h5.69l-1.72 1.72a.75.75 0 101.06 1.06l3-3z"
-              fill-rule="evenodd"
-            ></path>
-          </svg>
-        </button>
-      </Link>
-    </header>
+    <>
+      <header className="header">
+        <Link to="/" style={{ textDecoration: "none", alignSelf: "center" }}>
+          <img className="header__logo" src={logo} />
+        </Link>
+        <ul className="header__list">
+          <li
+            className={liMenuClassName}
+            onMouseEnter={() => setIsDropdown(true)}
+            onMouseLeave={() => setIsDropdown(false)}
+          >
+            MENU
+            {isDropdown && <Dropdown />}
+          </li>
+          <li className="header__list-item">ABOUT</li>
+          <li className="header__list-item">CONTACT US</li>
+          <li className="header__list-item">GALLERY</li>
+        </ul>
+        {/* <button className="header__button">BOOK ONLINE</button> */}
+        <Link to="/" style={{ textDecoration: "none", alignSelf: "center" }}>
+          <button className="header__button">
+            BOOK ONLINE
+            <svg fill="currentColor" viewBox="0 0 24 24" class="icon">
+              <path
+                clip-rule="evenodd"
+                d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm4.28 10.28a.75.75 0 000-1.06l-3-3a.75.75 0 10-1.06 1.06l1.72 1.72H8.25a.75.75 0 000 1.5h5.69l-1.72 1.72a.75.75 0 101.06 1.06l3-3z"
+                fill-rule="evenodd"
+              ></path>
+            </svg>
+          </button>
+        </Link>
+      </header>
+    </>
   );
 }
 
