bdb35f1de0715682238c46e7628d56e4d52feb11
Propitive <propitive@gmail.com>
2023-09-12 19:20:27 -0500

    added configuration for routes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,16 @@
+import { Route, Switch, useHistory } from "react-router-dom";
+
 import "./App.css";
 import Main from "../Main/Main";
 
 function App() {
-  return <Main />;
+  return (
+    <Switch>
+      <Route path="/">
+        <Main />
+      </Route>
+    </Switch>
+  );
 }
 
 export default App;
--- src/components/FeaturedSection/FeaturedSection.js
+++ src/components/FeaturedSection/FeaturedSection.js
@@ -40,7 +40,7 @@ function FeaturedSection() {
       </div>
       <div className="featured__text">
         <h3 className="featured__header">
-          Reshaping the way we think think about fire wood pizzas
+          Changing the way we think about fire wood pizzas
         </h3>
         <p className="featured__paragraph">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
@@ -50,6 +50,18 @@ function FeaturedSection() {
           reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
           pariatur.
         </p>
+        {/* <button className="featured__button">BOOK ONLINE</button> */}
+
+        <button className="featured__button">
+          BOOK ONLINE
+          <svg fill="currentColor" viewBox="0 0 24 24" class="icon">
+            <path
+              clip-rule="evenodd"
+              d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm4.28 10.28a.75.75 0 000-1.06l-3-3a.75.75 0 10-1.06 1.06l1.72 1.72H8.25a.75.75 0 000 1.5h5.69l-1.72 1.72a.75.75 0 101.06 1.06l3-3z"
+              fill-rule="evenodd"
+            ></path>
+          </svg>
+        </button>
       </div>
     </div>
   );
