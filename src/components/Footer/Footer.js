7d080b5c24d93ec1ad4c394f311d38db316534ce
Propitive <propitive@gmail.com>
2023-09-24 00:16:39 -0500

    footer formatting progress

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,50 +1,54 @@
-import facebookIcon from "../../images/icons/iconFacebook.png";
-import emailIcon from "../../images/icons/iconEmail.png";
+import facebookIcon from "../../images/icons/iconFacebook.svg";
+import emailIcon from "../../images/icons/iconEmail.svg";
 
 function Footer() {
   return (
     <footer className="footer">
-      <div className="footer__column">
-        <p className="footer__text">Home</p>
-        <p className="footer__text">About</p>
-        <p className="footer__text">Contact Us</p>
-        <p className="footer__text">Gallery</p>
-        <div className="footer__icons">
-          <a
-            href="https://github.com/propitive"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img
+      <div className="footer__container">
+        <div className="footer__column">
+          <p className="footer__text">Home</p>
+          <p className="footer__text">About</p>
+          <p className="footer__text">Contact Us</p>
+          <p className="footer__text">Gallery</p>
+          <div className="footer__icons">
+            <a
+              href="https://github.com/propitive"
+              target="_blank"
+              rel="noreferrer"
               className="footer__facebook"
-              src={facebookIcon}
-              alt="Facebook Icon"
-            ></img>
-          </a>
-          <a
-            href="https://github.com/propitive"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img
+            >
+              <img
+                className="footer__facebook"
+                src={facebookIcon}
+                alt="Facebook Icon"
+              ></img>
+            </a>
+            <a
+              href="https://github.com/propitive"
+              target="_blank"
+              rel="noreferrer"
               className="footer__email"
-              src={emailIcon}
-              alt="Email Icon"
-            ></img>
-          </a>
+            >
+              <img
+                className="footer__email"
+                src={emailIcon}
+                alt="Email Icon"
+              ></img>
+            </a>
+          </div>
         </div>
+        <div className="footer__column">
+          <p className="footer__text">Pizzas</p>
+          <p className="footer__text">Appetizers</p>
+          <p className="footer__text">Salads</p>
+          <p className="footer__text">Desserts</p>
+          <p className="footer__text">Pastas</p>
+        </div>
+        <div className="footer__column">
+          <p className="footer__text">Contact</p>
+        </div>
+        <p className="footer__author">Created by Jose Maldonado</p>
       </div>
-      <div className="footer__column">
-        <p className="footer__text">Pizzas</p>
-        <p className="footer__text">Appetizers</p>
-        <p className="footer__text">Salads</p>
-        <p className="footer__text">Desserts</p>
-        <p className="footer__text">Pastas</p>
-      </div>
-      <div className="footer__column">
-        <p className="footer__text">Contact</p>
-      </div>
-      <p className="footer__author">Created by Jose Maldonado</p>
     </footer>
   );
 }
